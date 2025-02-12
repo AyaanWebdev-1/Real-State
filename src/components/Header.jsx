20e71b56de3a2208ba7b67cf1459c40a932a1c91
AyaanWebdev-1 <ayanahmad.webdev@gmail.com>
2025-02-12 16:24:08 +0530

    third  commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,15 @@ import {motion} from 'framer-motion'
 
 function Header() {
   return (
-    <div className="min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden bg-image" style={{backgroundImage:"url('header_img.png')",}} id="header">
+    
+    <div className="min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden bg-image" style={{backgroundImage:"url('header-img.png')",}} id="header">
       <Navbar/>
       <motion.div
       initial={{opacity:0,y:100}}
       transition={{duration:1.5}}
       whileInView={{opacity:1,y:0}}
       viewport={{once:true}}
+      
       className="container text-center mx-auto py-4 px-6 lg:px-32 md:px-20 text-white  "> 
         <h2 className="text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20">Explores homes that fit your dream</h2>
         <div className="space-x-6 mt-16">
